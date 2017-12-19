40d990f38e3eda3d7b299b00a32bde347deac25a
Pasquale Spisto <spisto.pasquale1994@gmail.com>
2017-12-19 23:45:14 +0100

    Added RecognizerStartViewController linked to First View of recognizer

---
+++ moodplay/controllers/recognizer controllers/RecognizerStarViewController.swift
@@ -0,0 +1,16 @@
+//
+//  RecognizerStarViewController.swift
+//  moodplay
+//
+//  Created by Pasquale Spisto on 19/12/2017.
+//  Copyright © 2017 Pasquale Spisto. All rights reserved.
+//
+
+import UIKit
+
+class RecognizerStarViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+}
