48a9b29786392f00467ab327df4b0ce8371e5d1a
Andrea Mignano <andreamignano@10-20-51-78.0200.fed.it.iosda.org>
2017-12-20 18:47:08 +0100

    Fixed Recognition in Different Views. Now it works

--- moodplay/controllers/recognizer controllers/RecognitionViewController.swift
+++ moodplay/controllers/recognizer controllers/RecognitionViewController.swift
@@ -24,24 +24,9 @@ class RecognitionViewController: UIViewController {
     
     override func viewDidLoad() {
         super.viewDidLoad()
-
-        // Do any additional setup after loading the view.
+        self.sadMoodPercentage.text = String("\(Recognizer.shared.sadPercentage)%")
+        self.happyMoodPercentage.text = String("\(Recognizer.shared.happyPercentage)%")
+        self.motivatedMoodPercentage.text = String("\(Recognizer.shared.motivatedPercentage)%")
     }
-
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
-    }
-    
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destinationViewController.
-        // Pass the selected object to the new view controller.
-    }
-    */
 
 }
