8001de69e9ab2d897b22cbdb54b1d32cec98de82
Pasquale Pelliccia <pjspiderman@hotmail.it>
2017-12-20 21:47:50 +0100

    double pop added when done adding user moodlist

--- moodplay/controllers/CreatePlaylistViewController.swift
+++ moodplay/controllers/CreatePlaylistViewController.swift
@@ -43,6 +43,7 @@ class CreatePlaylistViewController: UIViewController, UINavigationControllerDele
         MoodlistDAO.shared.writeObjectToCloud(object: Moodlist(id: "", title: playlistName.text!, description: playlistDescription.text!, songs: [song], image: myImageView.image!))
         
         navigationController?.popViewController(animated: true)
+        navigationController?.popViewController(animated: true)
         
         
 
