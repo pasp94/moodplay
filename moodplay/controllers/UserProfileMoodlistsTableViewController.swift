3f450bdeee9186389a0c548086c9dffd998024f5
Pasquale Pelliccia <pjspiderman@hotmail.it>
2018-01-04 23:40:59 +0100

    added delete functionality of User's  moodlists

--- moodplay/controllers/UserProfileMoodlistsTableViewController.swift
+++ moodplay/controllers/UserProfileMoodlistsTableViewController.swift
@@ -10,7 +10,7 @@ import UIKit
 
 class UserProfileMoodlistsTableViewController: UITableViewController {
 
-    let moodlists = MoodlistDAO.shared.readAllObjects() as! [Moodlist]
+    var moodlists = MoodlistDAO.shared.readAllObjects() as! [Moodlist]
     override func viewDidLoad() {
         super.viewDidLoad()
 
@@ -67,25 +67,29 @@ class UserProfileMoodlistsTableViewController: UITableViewController {
     }
     
 
-    /*
+    
     // Override to support conditional editing of the table view.
     override func tableView(_ tableView: UITableView, canEditRowAt indexPath: IndexPath) -> Bool {
         // Return false if you do not want the specified item to be editable.
         return true
     }
-    */
+    
 
-    /*
+    
     // Override to support editing the table view.
     override func tableView(_ tableView: UITableView, commit editingStyle: UITableViewCellEditingStyle, forRowAt indexPath: IndexPath) {
         if editingStyle == .delete {
             // Delete the row from the data source
+            MoodlistDAO.shared.deleteRecord(id: moodlists[indexPath.row].id)
+            moodlists.remove(at: indexPath.row)
             tableView.deleteRows(at: [indexPath], with: .fade)
+            
+            
         } else if editingStyle == .insert {
             // Create a new instance of the appropriate class, insert it into the array, and add a new row to the table view
         }    
     }
-    */
+    
 
     /*
     // Override to support rearranging the table view.
